13e4bb3a17a00b76c1d9d245c2cad54b643e0acc
bsm <bsm@duostech.com>
2023-09-22 16:18:29 -0400

    Basic Template

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#![allow(unused)]
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+
+//Internal
+
+//External
+
+
+/**
+ * Use this codeBlock to describe the project.
+ */
+
+
+pub fn main() {
+    println!("Hello, world!");
+}
